d1d8561d8e2c4bb0f41bad588a62bce476556ab7
RaihanKabir277 <raihank0198@gmail.com>
2025-06-15 21:19:19 +0600

    basic higher order and event listener added here

--- higherOrder.py
+++ higherOrder.py
@@ -1,1 +1,16 @@
 
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+def move_forward():
+    timmy.forward(30)
+
+
+screen = Screen()
+screen.listen()
+screen.onkey(move_forward, "space")    #here move forward is a function it is work under another function , thats why its called higher order 
+
+
+screen.exitonclick()
+
